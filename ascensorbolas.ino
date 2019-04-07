837c3d0f7705e4f3161057ad0a075355b07b2d6a
candidogarcia <cgarci38@gmail.com>
2019-04-07 20:27:23 +0200

    Update ascensorbolas.ino

--- ascensorbolas.ino
+++ ascensorbolas.ino
@@ -1,28 +1,24 @@
-// Controlling a servo position using a potentiometer (variable resistor) 
-// by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
-
-#include <Servo.h> 
- 
 Servo myservo;  // create servo object to control a servo 
- Servo myservo1;  // create servo object to control a servo 
+Servo myservo1;  // create servo object to control a servo 
 
  
 void setup() 
 { 
-  myservo.attach(8);  // attaches the servo on pin 9 to the servo object 
-    myservo1.attach(4);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(8);  
+  myservo1.attach(4);  
 } 
  
 void loop() 
 { 
-     // scale it to use it with the servo (value between 0 and 180) 
-  myservo.write(180);                  // sets the servo position according to the scaled value 
+  myservo.write(180);                  
   delay(2000); 
-    myservo.write(0);                  // sets the servo position according to the scaled value 
-  delay(2000);  
-  myservo1.write(180);                  // sets the servo position according to the scaled value 
+ 
+  myservo.write(0);                  
+  delay(2000); 
+ 
+  myservo1.write(180);                 
   delay(2000);   
   
-    myservo1.write(0);                  // sets the servo position according to the scaled value 
-  delay(2000); // waits for the servo to get there 
+  myservo1.write(0);                  
+  delay(2000); 
 } 
